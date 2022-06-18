eceff9b158b1ed9e2a75b2c283e1e39bdb3c1dfc
Magnus Hansen <magnus.h1993@gmail.com>
2022-06-18 14:44:23 +0200

    Setting test instance on DBKover tests

--- junit5/src/main/kotlin/io/dbkover/junit5/annotation/DBKover.kt
+++ junit5/src/main/kotlin/io/dbkover/junit5/annotation/DBKover.kt
@@ -1,6 +1,7 @@
 package io.dbkover.junit5.annotation
 
 import io.dbkover.junit5.DBKoverExtension
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import java.lang.annotation.Inherited
 
@@ -9,4 +10,5 @@ import java.lang.annotation.Inherited
 @MustBeDocumented
 @Inherited
 @ExtendWith(DBKoverExtension::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 annotation class DBKover
