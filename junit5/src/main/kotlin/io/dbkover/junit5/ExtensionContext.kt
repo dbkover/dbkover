39fc864decaff3783da56a41bd33f1d83ac10b03
Casper Andresen <casper.andresen@cardlay.com>
2024-07-13 11:11:34 +0200

    feat: obtain connection from system / application properties

--- junit5/src/main/kotlin/io/dbkover/junit5/ExtensionContext.kt
+++ junit5/src/main/kotlin/io/dbkover/junit5/ExtensionContext.kt
@@ -1,9 +1,12 @@
 package io.dbkover.junit5
 
 import io.dbkover.junit5.annotation.DBKoverConnection
-import org.junit.jupiter.api.extension.*
-import org.junit.platform.commons.util.*
-import java.sql.*
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.platform.commons.util.AnnotationUtils
+import java.sql.Connection
+import java.sql.DriverManager
+import java.util.Properties
+
 
 internal fun <T : Annotation> ExtensionContext.hasAnnotation(annotation: Class<T>): Boolean =
     AnnotationUtils.isAnnotated(element, annotation)
@@ -23,7 +26,25 @@ internal fun ExtensionContext.findDataBaseConnectionFactory(): () -> Connection 
             connectionMethod.invoke(requiredTestInstance) as Connection?
                 ?: throw RuntimeException("Connection not initialized correctly")
         }
+    } else if (connectionMethod == null) {
+        return { obtainConnectionFromProperties() ?: throw RuntimeException("Connection not initialized correctly") }
     }
 
     throw RuntimeException("Connection method is missing")
+}
+
+private fun obtainConnectionFromProperties(): Connection? {
+    val url: String? = System.getProperty("datasources.default.url")
+    val username: String? = System.getProperty("datasources.default.username")
+    val password: String? = System.getProperty("datasources.default.password")
+
+    if (url == null || username == null || password == null) {
+        return null
+    }
+
+    val props = Properties()
+    props.setProperty("user", username)
+    props.setProperty("password", password)
+
+    return DriverManager.getConnection(url, props)
 }
