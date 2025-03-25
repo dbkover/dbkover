530a53685bb316624dcfc24c7f9c6202fa6df9df
Magnus Biolzi Hansen <magnus@biolzi.com>
2025-03-25 21:41:52 +0100

    fix: Updated dependencies

--- junit5/build.gradle.kts
+++ junit5/build.gradle.kts
@@ -7,17 +7,13 @@ tasks.test {
     useJUnitPlatform()
 }
 
-val junit_version: String by project
-
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     implementation(project(":core"))
 
-    implementation("org.junit.jupiter:junit-jupiter-api:$junit_version")
-    implementation("org.junit.jupiter:junit-jupiter-params:$junit_version")
-    implementation("org.junit.jupiter:junit-jupiter-engine:$junit_version")
+    implementation(libs.junit.api)
 
-    testImplementation("org.testcontainers:postgresql:1.17.2")
+    testImplementation(libs.testcontainers.postgres)
 }
